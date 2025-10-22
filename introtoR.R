f6d564ecb2cc169dc24fecdec45731f123ea38f2
farrelwahyudi <farrel.aryowahyudi@gmail.com>
2025-10-22 01:32:54 -0700

    Learn about initialization of variables, char, and array

--- introtoR.R
+++ introtoR.R
@@ -6,4 +6,17 @@
 3==3
 
 
+## Variables
+myWife <- "Matikanetannhauser"
+print(paste("The name of my wife is",myWife))
 
+monthName <- c("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+monthName[c(2,5,8)]
+print(paste(monthName[c(2,5,8)], collapse = ", ")) ### Print Month index 2,5,8 seperated by comma-space
+
+## How about if we want to initialize an array?
+umaName <- array(c("Special Week", "Oguri Cap", "Kitasan Black", "Symboli Rudolf", "Satono Diamond", "Rice Shower", "Matikanetannhauser", "Haru Urara", 
+                   "Daiwa Scarlet", "Tokai Teio", "Mejiro Mcqueen","Goldship"),dim = c(3,4))
+umaName
+umaName[1,] ### You can print one row or one column depending on your setting
+                 
